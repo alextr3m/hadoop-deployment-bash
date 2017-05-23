6c744138a40507a30ee055dcf3e1b8e104142828
Michael Arnold <github@razorsedge.org>
2017-05-22 22:24:36 -0400

    Fully disable IPv6.

--- disable_ipv6.sh
+++ disable_ipv6.sh
@@ -14,7 +14,7 @@
 #
 # Copyright Clairvoyant 2016
 
-exit 1
+DATE=`date +'%Y%m%d%H%M%S'`
 
 # Function to discover basic OS details.
 discover_os () {
@@ -48,12 +48,27 @@ if [ "$OS" != RedHatEnterpriseServer -a "$OS" != CentOS -a "$OS" != Debian -a "$
 fi
 
 if [ "$OS" == RedHatEnterpriseServer -o "$OS" == CentOS ]; then
+  echo "** Disabling IPv6 kernel configuration..."
   # https://access.redhat.com/solutions/8709
   # https://wiki.centos.org/FAQ/CentOS7#head-8984faf811faccca74c7bcdd74de7467f2fcd8ee
-  sysctl -w net.ipv6.conf.all.disable_ipv6=1
-  sysctl -w net.ipv6.conf.default.disable_ipv6=1
+  #sysctl -w net.ipv6.conf.all.disable_ipv6=1
+  #sysctl -w net.ipv6.conf.default.disable_ipv6=1
 
-  if [ "$OSREL" == 6 ]; then
+  if [ -d /etc/sysctl.d ]; then
+    if grep -q net.ipv6.conf.all.disable_ipv6 /etc/sysctl.conf; then
+      sed -i -e '/^net.ipv6.conf.all.disable_ipv6/d' /etc/sysctl.conf
+    fi
+    if grep -q net.ipv6.conf.default.disable_ipv6 /etc/sysctl.conf; then
+      sed -i -e '/^net.ipv6.conf.default.disable_ipv6/d' /etc/sysctl.conf
+    fi
+    echo "# Tuning for Hadoop installation." >/etc/sysctl.d/cloudera-ipv6.conf
+    echo "# CLAIRVOYANT" >>/etc/sysctl.d/cloudera-ipv6.conf
+    echo "net.ipv6.conf.all.disable_ipv6 = 1" >>/etc/sysctl.d/cloudera-ipv6.conf
+    echo "net.ipv6.conf.default.disable_ipv6 = 1" >>/etc/sysctl.d/cloudera-ipv6.conf
+    chown root:root /etc/sysctl.d/cloudera-ipv6.conf
+    chmod 0644 /etc/sysctl.d/cloudera-ipv6.conf
+    sysctl -p /etc/sysctl.d/cloudera-ipv6.conf
+  else
     if grep -q net.ipv6.conf.all.disable_ipv6 /etc/sysctl.conf; then
       sed -i -e "/^net.ipv6.conf.all.disable_ipv6/s|=.*|= 1|" /etc/sysctl.conf
     else
@@ -64,57 +79,111 @@ if [ "$OS" == RedHatEnterpriseServer -o "$OS" == CentOS ]; then
     else
       echo "net.ipv6.conf.default.disable_ipv6 = 1" >>/etc/sysctl.conf
     fi
-  else
-    if grep -q net.ipv6.conf.all.disable_ipv6 /etc/sysctl.conf; then
-      sed -i -e '/^net.ipv6.conf.all.disable_ipv6/d' /etc/sysctl.conf
-    fi
-    if grep -q net.ipv6.conf.default.disable_ipv6 /etc/sysctl.conf; then
-      sed -i -e '/^net.ipv6.conf.default.disable_ipv6/d' /etc/sysctl.conf
-    fi
-    echo "# Tuning for Hadoop installation." >/etc/sysctl.d/cloudera-ipv6.conf
-    echo "net.ipv6.conf.all.disable_ipv6 = 1" >>/etc/sysctl.d/cloudera-ipv6.conf
-    echo "net.ipv6.conf.default.disable_ipv6 = 1" >>/etc/sysctl.d/cloudera-ipv6.conf
+    sysctl -p /etc/sysctl.conf
   fi
 
-#mja needs work
-  #sed -e '/^AddressFamily/s|^AddressFamily .*|AddressFamily inet|' \
-  #    -e '/^#AddressFamily/a\
-  #AddressFamily inet' \
-  #    -e '/^ListenAddress/s|^ListenAddress.*|ListenAddress 0.0.0.0|' \
-  #    -e '/^#ListenAddress/a\
-  #ListenAddress 0.0.0.0' \
-  #    -i /etc/ssh/sshd_config
+  echo "** Disabling IPv6 kernel module..."
+  cat <<EOF >/etc/modprobe.d/cloudera-ipv6.conf
+# CLAIRVOYANT
+# Tuning for Hadoop installation.
+options ipv6 disable=1
+EOF
+  chown root:root /etc/modprobe.d/cloudera-ipv6.conf
+  chmod 0644 /etc/modprobe.d/cloudera-ipv6.conf
+
+  echo "** Disabling IPv6 in /etc/ssh/sshd_config..."
+  cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.${DATE}
   sed -e '/# CLAIRVOYANT$/d' \
       -e '/^AddressFamily /d' \
       -e '/^ListenAddress /d' \
       -i /etc/ssh/sshd_config
-  echo <<EOF >>/etc/ssh/sshd_config
+#mja needs work : assumes 0.0.0.0
+  cat <<EOF >>/etc/ssh/sshd_config
 # Hadoop: Disable IPv6 support # CLAIRVOYANT
 AddressFamily inet             # CLAIRVOYANT
 ListenAddress 0.0.0.0          # CLAIRVOYANT
 # Hadoop: Disable IPv6 support # CLAIRVOYANT
 EOF
+  service sshd restart
 
-#mja needs work
-  if rpm -q postfix; then
+  if rpm -q postfix >/dev/null; then
+    echo "** Disabling IPv6 in Postfix..."
+    cp -p /etc/postfix/main.cf /etc/postfix/main.cf.${DATE}
+#mja needs work : assumes 127.0.0.1
     postconf inet_interfaces
     postconf -e inet_interfaces=127.0.0.1
+    service postfix condrestart
   fi
 
-#mja needs work
   if [ -f /etc/netconfig ]; then
+    echo "** Disabling IPv6 in netconfig..."
+    cp -p /etc/netconfig /etc/netconfig.${DATE}
     sed -e '/inet6/d' -i /etc/netconfig
   fi
 
+  echo "** Disabling IPv6 in /etc/sysconfig/network..."
+  # https://github.com/fcaviggia/hardening-script-el6/blob/master/toggle_ipv6.sh
+  `grep -q IPV6INIT /etc/sysconfig/network`
+  if [ $? -ne 0 ]; then
+    echo "IPV6INIT=no" >> /etc/sysconfig/network
+  else
+    sed -i "/IPV6INIT/s/yes/no/" /etc/sysconfig/network
+  fi
+  for NET in $(ls /etc/sysconfig/network-scripts/ifcfg*); do
+    echo "** Disabling IPv6 in ${NET}..."
+    `grep -q IPV6INIT $NET`
+    if [ $? -ne 0 ]; then
+      echo "IPV6INIT=no" >> $NET
+    else
+      sed -i "/IPV6INIT/s/yes/no/" $NET
+    fi
+  done
+  echo "** Unloading IPv6 kernel module..."
+  rmmod ipv6 &>/dev/null
+  echo "** Stopping IPv6 firewall..."
   service ip6tables stop
   chkconfig ip6tables off
+
 elif [ "$OS" == Debian -o "$OS" == Ubuntu ]; then
+  echo "** Disabling IPv6 kernel configuration..."
+  if grep -q net.ipv6.conf.all.disable_ipv6 /etc/sysctl.conf; then
+    sed -i -e '/^net.ipv6.conf.all.disable_ipv6/d' /etc/sysctl.conf
+  fi
+  if grep -q net.ipv6.conf.default.disable_ipv6 /etc/sysctl.conf; then
+    sed -i -e '/^net.ipv6.conf.default.disable_ipv6/d' /etc/sysctl.conf
+  fi
+  if grep -q net.ipv6.conf.lo.disable_ipv6 /etc/sysctl.conf; then
+    sed -i -e '/^net.ipv6.conf.lo.disable_ipv6/d' /etc/sysctl.conf
+  fi
   # https://askubuntu.com/questions/440649/how-to-disable-ipv6-in-ubuntu-14-04
   echo "# Tuning for Hadoop installation." >/etc/sysctl.d/cloudera-ipv6.conf
-  echo "net.ipv6.conf.all.disable_ipv6 = 1" >/etc/sysctl.d/cloudera-ipv6.conf
-  echo "net.ipv6.conf.default.disable_ipv6 = 1" >/etc/sysctl.d/cloudera-ipv6.conf
-  echo "net.ipv6.conf.lo.disable_ipv6 = 1" >/etc/sysctl.d/cloudera-ipv6.conf
-  sysctl -p
-#mja needs work
+  echo "# CLAIRVOYANT" >>/etc/sysctl.d/cloudera-ipv6.conf
+  echo "net.ipv6.conf.all.disable_ipv6 = 1" >>/etc/sysctl.d/cloudera-ipv6.conf
+  echo "net.ipv6.conf.default.disable_ipv6 = 1" >>/etc/sysctl.d/cloudera-ipv6.conf
+  echo "net.ipv6.conf.lo.disable_ipv6 = 1" >>/etc/sysctl.d/cloudera-ipv6.conf
+  chown root:root /etc/sysctl.d/cloudera-ipv6.conf
+  chmod 0644 /etc/sysctl.d/cloudera-ipv6.conf
+  service procps start
+
+  echo "** Disabling IPv6 in /etc/ssh/sshd_config..."
+  cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.${DATE}
+  sed -e '/# CLAIRVOYANT$/d' \
+      -e '/^AddressFamily /d' \
+      -e '/^ListenAddress /d' \
+      -i /etc/ssh/sshd_config
+#mja needs work : assumes 0.0.0.0
+  cat <<EOF >>/etc/ssh/sshd_config
+# Hadoop: Disable IPv6 support # CLAIRVOYANT
+AddressFamily inet             # CLAIRVOYANT
+ListenAddress 0.0.0.0          # CLAIRVOYANT
+# Hadoop: Disable IPv6 support # CLAIRVOYANT
+EOF
+  service ssh restart
+
+  if [ -f /etc/netconfig ]; then
+    echo "** Disabling IPv6 in netconfig..."
+    cp -p /etc/netconfig /etc/netconfig.${DATE}
+    sed -e '/inet6/d' -i /etc/netconfig
+  fi
 fi
 
